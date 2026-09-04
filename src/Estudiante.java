bf387a6c11029137fd65cbf77fb36c7b2e9cd60b
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 21:42:45 -0400

    JAVA-003 - Agregar constructor de Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -6,6 +6,11 @@ public class Estudiante{
     private String carrera;
     private int edad;
 
-}
+    public Estudiante(String nombre, String carrera, int edad) {
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.edad = edad;
+    }
 
+}
 
